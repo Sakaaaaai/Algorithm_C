03986a3fc096561a617d9d70377b562172a7b104
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-09 00:44:38 +0900

    Create Depth_First_Search.c

---
+++ Depth_First_Search.c
@@ -0,0 +1,90 @@
+/*
+input
+4
+1 1 2
+2 1 4
+3 0
+4 1 3
+
+output
+1 1 8
+2 2 7
+3 4 5
+4 3 6
+*/
+
+
+#include <stdio.h>
+
+#define WHITE 10
+#define GRAY 20
+#define BLACK 30
+
+#define N 100
+#define N_2 200
+
+int time = 0;
+
+int d[N_2],f[N_2];
+int color[N], u[N], outdegree[N];
+int v[N][N];
+int n;
+
+void visit(int u){
+    int i = 0; 
+    color[u] = GRAY;
+    d[u] = ++time;
+    for(i = 0;i < outdegree[u]; i++){
+        if(color[v[u][i]] == WHITE){
+        visit(v[u][i]);    
+        }
+    }
+    color[u] = BLACK;
+    f[u] = ++time;
+}
+
+
+void Print(int n){
+
+int i;
+
+for(i = 1; i<=n; i++){
+    printf("%d %d %d\n",i,d[i],f[i] );
+ }
+}
+
+
+int main(void){
+
+int i, j;
+int n;
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++){
+
+    scanf("%d",&u[i]);
+
+    color[u[i]] = WHITE;
+
+    scanf("%d",&outdegree[u[i]]);
+
+    for(j = 0;j< outdegree[u[i]]; j++ ){
+
+        scanf("%d",&v[u[i]][j]);
+
+    }
+}
+
+for(i = 1;i<=n;i++){
+    if(color[i] == WHITE){
+        visit(i);
+    }
+}
+
+Print(n);
+
+return 0;
+
+}
+
