519f99d8babc25903bcd30baf2d79960a93791cd
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 21:35:03 +0900

    Create Linear_Search.c

---
+++ Linear_Search.c
@@ -0,0 +1,38 @@
+/*
+input
+5
+1 1 2 2 3
+2
+1 2
+
+output
+2
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int array_1[10000],array_2[500], i, j, n_1, n_2, count=0;
+  
+  scanf("%d",&n_1);
+  for(i=0;i<n_1;i++)scanf("%d",&array_1[i]);
+
+  scanf("%d",&n_2);
+  for(j=0;j<n_2;j++)scanf("%d",&array_2[j]);
+
+  for(j=0;j<n_2;j++){
+    for(i=0;i<n_1;i++){
+      
+      if(array_2[j] == array_1[i]){
+	count++;
+	break;	
+      }
+    }
+  }
+  
+  printf("%d\n",count);
+  
+  return 0;
+}
+
