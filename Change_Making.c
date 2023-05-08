3f6e9432570efa2522e89c93b4c69f9ac9c903bf
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-09 01:17:41 +0900

    Create Change_Making.c

---
+++ Change_Making.c
@@ -0,0 +1,34 @@
+/*
+input
+54321
+
+output
+2175
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+	int money;
+	int coin[4] = {1, 5, 10, 25};
+	int sum, tmp;
+	int i;
+
+	scanf("%d", &money);
+
+
+	sum = 0;
+	for(i = 0; i < 4; i++){
+		if( (tmp = money / coin[3 - i]) ){
+			sum += money / coin[3 - i];
+			money %= coin[3 - i];
+		}
+	}
+
+
+	printf("%d\n", sum);
+
+	return 0;
+}
