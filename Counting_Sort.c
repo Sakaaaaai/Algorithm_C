5c776ed14f821b622dbf7f86ab2658b6cc927ada
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 23:15:40 +0900

    Create Counting_Sort.c

---
+++ Counting_Sort.c
@@ -0,0 +1,49 @@
+/*
+input
+7
+2 5 1 3 2 3 0
+
+output
+0 1 2 2 3 3 5
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int array[10000];
+
+void print(int n){
+int i, j, count=0;
+
+for(i=0;i<10000;i++)
+    {
+        for(j=0;j<array[i];j++)
+        {
+            printf("%d",i);
+            if(count<n-1){
+                printf(" ");
+                count++;
+            }
+        }
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    
+    int n,i,b,j,c=0;
+
+    scanf("%d",&n);
+    for(i=0;i<10000;i++){
+        array[i]=0;
+    }
+    for(i=0;i<n;i++){
+        scanf("%d",&b);
+        array[b]++;
+    }
+print(n);
+    return 0;
+}
+
