7a7822770f29b036a1b212bd50a0e442929873eb
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 21:23:20 +0900

    Create Queue.c

---
+++ Queue.c
@@ -0,0 +1,91 @@
+/*
+input
+5 100
+p1 150
+p2 80
+p3 200
+p4 350
+p5 20
+
+output
+p2 180
+p5 400
+p1 450
+p3 550
+p4 800
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define LEN 100001
+
+typedef struct p{
+  char name[10];
+  int time;
+}data;
+
+
+void enqueue(data);
+data dequeue(void);
+  
+data Q[LEN];
+int head, tail, n;
+
+int main(){
+  
+  int lap_time = 0, min, i, q;
+  data x;
+
+  head = 1;
+
+  scanf("%d%d", &n, &q);
+
+  for (i = 1; i < n+1; i++)scanf("%s %d", Q[i].name, &Q[i].time);
+    
+  tail = n+1;
+
+  while(head != tail){
+    x = dequeue();
+    
+    if(q <= x.time)min = q;
+    else min = x.time;
+    
+    x.time = x.time - min;
+    
+    lap_time = lap_time + min;
+    
+    if(x.time > 0){
+      enqueue(x);
+    }
+    
+    else{
+      printf("%s %d\n",x.name ,lap_time);
+    }
+    
+  }
+  
+  return 0;
+  
+}
+
+
+void enqueue(data x){
+  
+  Q[tail] = x;
+  
+  tail = (tail + 1)%(LEN - 1);
+}
+
+
+data dequeue(void){
+  data x;
+  
+  x = Q[head];
+  
+  head = (head + 1)%(LEN - 1);
+  
+  return x;
+}
+
+
