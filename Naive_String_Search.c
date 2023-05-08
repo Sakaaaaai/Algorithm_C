fcd7bf7eba4fa023648c5f8927c20c678c6018c0
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-09 01:08:27 +0900

    Create Naive_String_Search.c

---
+++ Naive_String_Search.c
@@ -0,0 +1,33 @@
+/*
+input
+aabaaa
+aa
+
+output
+0
+3
+4
+*/
+
+#include <stdio.h>          
+#include <string.h>       
+
+int
+main(int argc, char** argv)
+{
+	char t[1001];
+	char p[1001];
+	int m, n;
+	int i;
+
+	scanf("%s %s", t, p);
+	m = strlen(t);
+	n = strlen(p);
+	for (i = 0; i <= m - n; ++i)
+	{
+		if (strncmp(&t[i], p, n) == 0)
+			printf("%d\n", i);
+	}
+
+	return 0;
+}
