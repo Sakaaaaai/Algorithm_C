00bf5de67c6b6f6800e42b037c4222d0546d3307
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-09 00:49:10 +0900

    Create Connected_Components.c

---
+++ Connected_Components.c
@@ -0,0 +1,79 @@
+/*
+input
+10 9
+0 1
+0 2
+3 4
+5 7
+5 6
+6 7
+6 8
+7 8
+8 9
+3
+0 1
+5 9
+1 3
+
+output
+yes
+yes
+no
+*/
+
+#include<stdio.h>
+
+#define N 100000
+
+int A[N];
+
+void res(int a,int b);
+int sum(int a, int b);
+int search(int p);
+
+int main()
+{
+
+  int n,m,i,a,b,c;
+
+  for(i=0; i<N; i++) A[i]=i;
+
+  scanf("%d%d",&n,&m);
+
+  for(i=0; i<m; i++){
+    scanf("%d%d",&a,&b);
+    res(a,b);
+  }
+
+  scanf("%d",&c);
+  for(i=0; i<c; i++){
+    scanf("%d%d",&a,&b);
+    if(sum(a,b)==1)printf("yes\n");
+    else printf("no\n");
+  }
+  return 0;
+}
+
+void res(int a, int b)
+{
+
+  a=search(a);
+  b=search(b);
+
+  if(a==b)return;
+
+  A[b]=a;
+}
+
+int sum(int a, int b)
+{
+
+  if(search(a)==search(b))return 1;
+  else return 0;
+}
+int search(int p)
+{
+  if(p==A[p])return p;
+  return A[p]=search(A[p]);
+}
+
