1cc1a1e16badd44e9bebd20d2c80168c109a4c7d
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 23:28:30 +0900

    Create Reconstruction_of_a_Tree.c

---
+++ Reconstruction_of_a_Tree.c
@@ -0,0 +1,56 @@
+/*
+input
+5
+1 2 3 4 5
+3 2 4 1 5
+
+output
+3 4 2 5 1
+*/
+
+#include <stdio.h>
+
+#define MAX 40
+
+void solve(int*,int*,int);
+
+int n, post[MAX], idx;
+
+int main(){
+  int pre[MAX], in[MAX], i;
+
+  scanf("%d",&n);
+  for(i = 0 ; i < n ; i++){
+    scanf("%d",pre+i);
+  }
+  for(i = 0 ; i < n ; i++){
+    scanf("%d",in+i); 
+  }
+
+  solve(pre, in, n);
+
+  for(i = 0 ; i < n ; i++){
+    printf("%d%c",post[i],(i != n-1 ? ' ':'\n'));
+  }
+
+  return 0;
+}
+
+void solve(int pre[], int in[], int N){
+  int i, p;
+
+  for(i = 0 ; i < N ; i++){
+    if(in[i] == pre[0]){
+      p = i;
+      break;
+    }
+  }
+
+  if(p != 0){
+    solve(pre+1, in, p);
+  }
+  if(p != N-1){
+    solve(pre+p+1, in+p+1, N-p-1);
+  }
+  post[idx++] = pre[0]; 
+}
