e952b8abcbc05c3294fc10d1c5ca3f50a1fdcb18
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 23:51:26 +0900

    Create Fibonacci_Number.c

---
+++ Fibonacci_Number.c
@@ -0,0 +1,34 @@
+/*
+input
+3
+
+output
+3
+*/
+
+
+#include<stdio.h>
+
+#define N 45
+
+int data[N];
+
+int Fibonacci(int n)
+{
+	if (n == 0 || n == 1) return data[n] = 1;
+
+	if (data[n]) return data[n];
+
+	else return data[n] = (Fibonacci(n - 1) + Fibonacci(n - 2));
+}
+
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+
+	printf("%d\n", Fibonacci(n));
+
+    return 0;
+}
