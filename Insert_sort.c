6e90b78ffcde1efe342e7e1419288ff96419212c
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 18:31:41 +0900

    Create Insert_sort.c

---
+++ Insert_sort.c
@@ -0,0 +1,57 @@
+/*input
+6
+5 2 4 6 1 3
+
+output
+5 2 4 6 1 3
+2 5 4 6 1 3
+2 4 5 6 1 3
+2 4 5 6 1 3
+1 2 4 5 6 3
+1 2 3 4 5 6
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+  
+  int main()
+{
+  int n, i=0, j=0, array[1000], k, key;
+  
+  scanf("%d",&n);
+
+  for(i=0;i<n;i++)
+    {
+      scanf("%d",&array[i]);
+    }
+
+ for(k=0;k<n-1;k++)printf("%d ",array[k]);
+ printf("%d",array[n-1]);
+  printf("\n");
+  
+  for(i=1;i<n;i++)
+    {
+      key = array[i];
+      j = i - 1;
+      
+      while(j >= 0 && array[j] > key)
+	{
+	  array[j+1] = array[j];
+	  j--;
+	}
+      
+      array[j+1] = key;
+      
+      for(k=0;k<n-1;k++)printf("%d ",array[k]);
+      printf("%d",array[n-1]);
+      printf("\n");
+ 
+    }
+   
+  return 0;
+}
+
+
+
