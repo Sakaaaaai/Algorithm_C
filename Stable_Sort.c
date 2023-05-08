a9e6f02007ac184abc934fdfd5a98f4b44b5d30a
Sakaaaaai <132359636+Sakaaaaai@users.noreply.github.com>
2023-05-08 19:17:26 +0900

    Create Stable_Sort.c

---
+++ Stable_Sort.c
@@ -0,0 +1,143 @@
+/*
+input
+5
+H4 C9 S4 D2 C3
+
+output
+D2 C3 H4 S4 C9
+Stable
+D2 C3 S4 H4 C9
+Not stable
+*/
+
+#include <stdio.h>
+
+
+typedef struct{
+  
+  int number;
+  char ch;
+  
+} card;
+
+
+void Bubble(card *, int);
+
+void Selection(card *, int);
+
+
+int main(){
+  int i, j, flag = 0, data, minj;
+
+  scanf("%d", &data);
+  
+  card Bub[data], Sel[data];
+  
+  for(i=0; i<data; i++)
+    {
+      
+    scanf(" %c%d", &Bub[i].number, &Bub[i].ch);
+    
+    Sel[i] = Bub[i];
+    
+  }
+ 
+  Bubble(Bub, data);
+  
+  printf("Stable\n");
+  
+  Selection(Sel, data);
+  
+  for(i=0; i<data; i++)
+    {
+      
+      if(Bub[i].number != Sel[i].number)
+	{
+	  flag++;
+	}
+    }
+  
+  
+  if(flag>0)
+    {
+      printf("Not stable\n");
+    }
+  
+  else  printf("Stable\n");
+  
+  return 0;
+  
+}
+
+
+void Bubble(card *Bub,  int data){
+  
+  int i, j, minj;
+  card tmp;
+  
+  for(i=0; i<data; i++)
+    {
+      
+    for(j=data-1; j>i; j--)
+      {
+	
+      if(Bub[j].ch < Bub[j-1].ch)
+	{
+	  
+	tmp = Bub[j];
+	Bub[j] = Bub[j-1];
+	Bub[j-1] = tmp;
+	
+      }
+    }
+  }
+  
+  for(i = 0; i < data-1; i++)
+    {
+      printf("%c%d", Bub[i].number, Bub[i].ch);
+      printf(" ");  
+    }
+  
+  printf("%c%d", Bub[i].number, Bub[i].ch);
+  printf("\n");
+  
+}
+
+
+void Selection(card *Sel, int data){
+  
+  int i, j, a, minj;
+  card tmp;
+  
+  for(i=0; i<data; i++)
+    {
+      minj = i;
+      
+      for(j=i; j<data; j++)
+	{
+	  
+	  if(Sel[j].ch < Sel[minj].ch)
+	    {
+	      minj=j;
+	    }
+      
+	}
+    
+      tmp = Sel[i];
+      Sel[i] = Sel[minj];
+      Sel[minj] = tmp;
+    
+    }
+
+  for(i = 0; i < data-1; i++)
+    {      
+      printf("%c%d", Sel[i].number, Sel[i].ch);         
+      printf(" ");	
+    }
+  
+  printf("%c%d", Sel[i].number, Sel[i].ch);
+  
+  printf("\n");
+  
+}
+
